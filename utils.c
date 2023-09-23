2566870cbc0b095b83aed4aceb78fc91f4f283ad
migmanu42 <jmanuelmigoya@gmail.com>
2023-09-23 14:13:12 +0200

    Initial commit

---
+++ utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 14:12:03 by jmigoya-          #+#    #+#             */
+/*   Updated: 2023/09/23 14:12:54 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	get_path(char *cmd, char *env[])
+{
+
+}
+
+void	exec(char *cmd, char *env[])
+{
+
+}
+
+void	error(void)
+{
+		perror("error");
+		exit(EXIT_FAILURE);
+}
+
