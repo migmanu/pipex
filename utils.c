bb28605ca563265bafb81597c85a308476783d2d
migmanu42 <jmanuelmigoya@gmail.com>
2023-09-23 15:05:23 +0200

    Finish get_path()

--- utils.c
+++ utils.c
@@ -6,15 +6,38 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 14:12:03 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/09/23 14:12:54 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/09/23 15:05:01 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	get_path(char *cmd, char *env[])
+char	*get_path(char *cmd, char *env[])
 {
+	char	**path_vec;
+	char	*tmp_path;
+	char	*path;
+	int		i;
 
+	i = 0;
+	while (ft_strnstr(env[i], "PATH", 4) == 0)
+		i++;
+	path_vec = ft_split(env[i] + 5, ':');
+	i = 0;
+	while (path_vec[i])
+	{
+		tmp_path = ft_strjoin("/", cmd);
+		path = ft_strjoin(path_vec[i], tmp_path);
+		free(tmp_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = 0;
+	while (path_vec[i])
+		free(path_vec[i++]);
+	free(path_vec);
 }
 
 void	exec(char *cmd, char *env[])
@@ -28,3 +51,8 @@ void	error(void)
 		exit(EXIT_FAILURE);
 }
 
+int	main(int argc, char *argv[], char *env[])
+{
+	get_path("ls", env);
+	return (0);
+}
