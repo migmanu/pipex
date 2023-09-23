4dcab13ff48b84cb859ed677d8227fc413cf2747
migmanu42 <jmanuelmigoya@gmail.com>
2023-09-23 18:38:17 +0200

    Add open safeguard at start of main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 17:26:47 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/09/23 14:12:47 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/09/23 18:37:10 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ void	child(char *argv[], int fd[], char *env[])
 	if (filein == -1)
 		error();
 	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
 	dup2(filein, STDIN_FILENO);
+	close(filein);
 	close(fd[0]);
-	//execute
+	exec(argv[2], env);
 }
 
 void	parent(char *argv[], int fd[], char *env[])
@@ -32,24 +34,31 @@ void	parent(char *argv[], int fd[], char *env[])
 	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fileout == -1)
 		error();
-	dup2(fd[0], STDIN_FILENOL);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
 	dup2(fileout, STDOUT_FILENO);
+	close(fileout);
 	close(fd[1]);
-	// execute
+	exec(argv[3], env);
 }
 
 int main(int argc, char *argv[], char *env[])
 {
 	int		fd[2];
+	int		test_file;
 	pid_t	pid;
 
+	test_file = open(argv[1], 0, 0);
+	if (test_file < 0)
+		error();
+	close(test_file);
 	if (argc != 5)
-		return (2);
+		error();
 	if (pipe(fd) == -1)
-		return (2);
+		error();
 	pid = fork();
 	if (pid == -1)
-		return (2);
+		error();
 	if (pid == 0)
 		child(argv, fd, env);
 	else
